c039733efedd21ba2720adee4a0398f35d4e589e
away-2 <1968642178@qq.com>
2023-03-09 16:39:23 +0800

    横向滚动及回到顶部

---
+++ cainiao/README.md
@@ -0,0 +1,7 @@
+#  横向带indicator 的滚动菜单组件封装
+
+- 滚动的第一要求
+    content 超出wrapper 高度/宽度
+- 让页面滚动更高级
+    1. 把原来的scroll 滚动条  干掉
+    2. better-scroll  
---
+++ cainiao/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <horizen-scroll-menu />
+ <back-top :showHeight="showHeight">
+  BackTop
+ </back-top>
+<div style="height: 3000px;"></div>
+</template>
+
+<script setup>
+import { ref,onMounted } from 'vue'
+import HorizenScrollMenu from './components/HorizenScrollMenu.vue'
+import BackTop from './components/BackTop.vue'
+
+const showHeight = ref(0)
+onMounted(() =>{
+  showHeight.ref.value = window.innerHeight
+  // console.log(showHeight)
+})
+</script>
+
+<style lang="stylus" scoped>
+*
+  margin 0
+  padding 0
+</style>
