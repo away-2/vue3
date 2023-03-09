538c2d0bcf94588da1b5ef7190c6aa6f2a4ba2c7
away-2 <1968642178@qq.com>
2023-03-09 21:24:52 +0800

    商城管理后台

---
+++ mall-admin/src/router/index.js
@@ -0,0 +1,60 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',   // 根路径   login 
+            // http 状态码  302 跳转 浏览器 
+            redirect: '/introduce'  // 跳转
+        },
+        {
+            path: '/introduce',
+            name: 'introduce',
+            component: () => import('@/views/Introduce.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/Login.vue')
+        },
+        {
+            path: '/add',
+            name: 'add',
+            meta: {
+                login: true
+            },
+            component: () => import('@/views/Add.vue')
+        },
+        {
+            path:'/swiper',
+            name: 'swiper',
+            meta: {
+                login: true
+            },
+            component: () => import('@/views/Swiper.vue')
+        },
+        {
+            path: '/category',
+            name: 'category',
+            meta: {
+                login: true
+            },
+            component: () => import('@/views/Category.vue'),
+            children: [
+                {
+                    path: '/category/level2',
+                    name: 'level2',
+                    component: () => import('@/views/Category.vue')
+                },
+                {
+                    path: '/category/level3',
+                    name: 'level3',
+                    component: () => import('@/views/Category.vue')
+                }
+            ]
+        }
+    ]
+})
+
+export default router
---
+++ mall-admin/src/views/Add.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Add
+    {{profile.loginUserName}}------{{ profile.nickName }}
+  </div>
+</template>
+
+<script setup>
+import { computed } from '@vue/reactivity';
+import { useUserStore} from '@/store/user.js'
+// 如何从中央去profile 到页面组件?
+const userStore = useUserStore(); // useRouter hooks
+console.log(userStore.profile.loginUserName, '????');
+
+const profile = computed(() => userStore.profile)
+console.log(profile, '啊啊啊好烦啊啊啊啊');
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
