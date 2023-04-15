c955d52aa3e36b1be791c836540725f0d2710ef1
away-2 <1968642178@qq.com>
2023-04-16 00:40:16 +0800

    详情页

--- mall/src/router/index.js
+++ mall/src/router/index.js
@@ -2,21 +2,23 @@
 // 路由类型的设置
 // 数组的配置
 // 路由的懒加载
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {
+    createRouter,
+    createWebHashHistory
+} from 'vue-router'
 import Home from '../views/Home.vue' // 页面级别组件  路径 ../ 相对
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: [
-        {
-            path: '/',   // 根路径   login 
+    routes: [{
+            path: '/', // 根路径   login 
             // http 状态码  302 跳转 浏览器 
-            redirect: '/home'  // 跳转
+            redirect: '/home' // 跳转
         },
         {
             path: '/home',
             name: 'home',
-            meta:{
+            meta: {
                 index: 1
             },
             component: Home
@@ -45,11 +47,19 @@ const router = createRouter({
             // 动态路由   url  params   ?id= queryString
             path: '/detail/:id',
             name: 'detail',
-            meta:{
+            meta: {
                 index: 3
             },
             component: () => import('@/views/Detail.vue')
-        }
+        },
+        {
+            path: '/product-list',
+            name: 'product-list',
+            meta: {
+                index: 2
+            },
+            component: () => import('@/views/ProductList.vue')
+        },
     ]
 })
 
--- mall/src/views/Detail.vue
+++ mall/src/views/Detail.vue
@@ -31,6 +31,12 @@
                 <div class="product-content" v-html=" state.detail.goodsDetailContent "></div>
             </div>
         </div>
+        <van-action-bar>
+            <van-action-bar-icon icon="chat-o" text="客服"></van-action-bar-icon>
+            <van-action-bar-icon icon="cart-o" text="购物车" :badge="3"></van-action-bar-icon>
+            <van-action-bar-button type="warning" text="加入购物车"></van-action-bar-button>
+            <van-action-bar-button type="danger" text="立即购买"></van-action-bar-button>
+        </van-action-bar>
     </div>
 </template>
 
@@ -40,21 +46,79 @@ import { useRoute } from 'vue-router'
 import { getDetail } from '@/service/detail'
 import SimpleHeader from '../components/SimpleHeader.vue'
 // router 是所有的route 集合
+import { showLoadingToast, closeToast } from 'vant'
 
 const route = useRoute(); // 当前的路由
 const state = reactive({
-    detail: {}
+    detail: {},
+    loading: true
 })
 
 onMounted(async () => {
     // console.log(route);
     const { id } = route.params 
+    showLoadingToast({
+      message: '加载中...',
+      forbidClick: true
+  })
     const { data} = await getDetail(id)  // 异步
     state.detail = data 
+    closeToast()
 })
 
 </script>
 
 <style lang="stylus" scoped>
-
+@import '../common/style/mixin'
+.product-detail
+    .detail-content
+        height calc(100vh - 1.33333rem)
+        overflow hidden
+        overflow-y auto
+        .detail-swipe-wrap
+            .my-swipe
+                img
+                    width 100%
+        .product-info
+            padding 0 .26667rem
+            .product-title
+                font-size 0.48rem
+                text-align-last left
+                color #333
+            .product-desc
+                font-size .37333rem
+                text-align-last left
+                color #999
+                padding .13333rem 0
+            .product-price
+                fj()
+                span:nth-child(1)
+                    color #f63515
+                    font-size .58667rem
+                span:nth-child(2)
+                    color #999
+                    font-size .426667rem
+    .product-intro
+        width 100%
+        padding-bottom 1.3333em
+        ul
+            fj()
+            width 100%
+            margin 0.2667rem 0
+            li
+                flex 1
+                padding 0.1333rem 0
+                text-align center
+                font-size .4rem
+                border-right 1px solid #999
+                &:last-child
+                    border-right none
+        .product-content
+            padding 0 .266667rem
+            img
+                width 100%
+.van-action-bar-button--warning
+    background linear-gradient(to right, #6bd9d9, $primary)
+.van-action-bar-button--danger
+    background linear-gradient(to right, #0dc3c3, #098888)
 </style>
