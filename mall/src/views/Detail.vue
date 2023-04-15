6b69ac5d19bca061fa17b95e46b1878c2ddc45ad
away-2 <1968642178@qq.com>
2023-04-15 23:40:50 +0800

    v-html

--- mall/src/views/Detail.vue
+++ mall/src/views/Detail.vue
@@ -1,7 +1,36 @@
 <template>
     <div class="product-detail">
         <simple-header name="商品详情"></simple-header>
-
+        <div class="detail-content">
+            <div class="detail-swipe-wrap">
+                <van-swipe class="my-swipe" indicator="#1baeae">
+                    <van-swipe-item
+                        v-for="(item,index) in state.detail.goodsCarouselList"
+                        :key="index" 
+                    >
+                        <img v-lazy="item" alt="">
+                    </van-swipe-item>
+                </van-swipe>
+            </div>
+            <div class="product-info">
+                <div class="product-title">
+                    {{ state.detail.goodsName }}
+                </div>
+                <div class="product-desc">免邮费 顺丰快递</div>
+                <div class="product-price">
+                    <span>￥{{ state.detail.sellingPrice }}</span>
+                </div>
+            </div>
+            <div class="product-intro">
+                <ul>
+                    <li>概述</li>
+                    <li>参数</li>
+                    <li>安装服务</li>
+                    <li>常见问题</li>
+                </ul>
+                <div class="product-content" v-html=" state.detail.goodsDetailContent "></div>
+            </div>
+        </div>
     </div>
 </template>
 
